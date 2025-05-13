088b7ac49fdba7e9b5daa8428e60b5ba3408ea76
J. S. <78266847+WaffleSS111@users.noreply.github.com>
2025-05-13 16:42:05 +0200

    updated data file
    More events

--- data/events.js
+++ data/events.js
@@ -2,45 +2,73 @@
 let events = [
   {
     id: 1,
-    name: "Concert",
-    date: "2023-03-01",
+    name: "Rock Concert",
+    date: "2025-03-01",
     time: "19:00",
-    location: "New York"
+    location: "Cape Town International Convention Centre (CTICC) - Cape Town"
   },
   {
     id: 2,
-    name: "Theater Play",
-    date: "2023-03-15",
+    name: "Comedy Night",
+    date: "2025-03-15",
     time: "20:00",
-    location: "Los Angeles"
+    location: "Gallagher Convention Centre - Midrand, Johannesburg"
   },
   {
     id: 3,
-    name: "Sports Game",
-    date: "2023-03-20",
+    name: "Theater Play",
+    date: "2025-03-20",
     time: "18:00",
-    location: "Chicago"
+    location: "Durban International Convention Centre (Durban ICC) - Durban"
   },
   {
     id: 4,
-    name: "Festival",
-    date: "2023-04-01",
+    name: "Music Festival",
+    date: "2025-04-01",
     time: "12:00",
-    location: "Miami"
+    location: "Sandton Convention Centre - Johannesburg"
   },
   {
     id: 5,
-    name: "Conference",
-    date: "2023-04-10",
+    name: "Dance Performance",
+    date: "2025-04-10",
     time: "09:00",
-    location: "San Francisco"
+    location: "Ticketpro Dome - Johannesburg"
   },
   {
     id: 6,
-    name: "Comedy Show",
-    date: "2023-04-15",
+    name: "Film Premiere",
+    date: "2025-04-15",
     time: "21:00",
-    location: "Boston"
+    location: "Artscape Theatre Centre - Cape Town"
+  },
+  {
+    id: 7,
+    name: "Art Exhibition",
+    date: "2025-04-20",
+    time: "10:00",
+    location: "Nelson Mandela Bay Stadium - Port Elizabeth"
+  },
+  {
+    id: 8,
+    name: "Gaming Tournament",
+    date: "2025-04-25",
+    time: "14:00",
+    location: "Loftus Versfeld Stadium - Pretoria"
+  },
+  {
+    id: 9,
+    name: "Food Festival",
+    date: "2025-05-01",
+    time: "11:00",
+    location: "Montecasino Events Centre - Fourways, Johannesburg"
+  },
+  {
+    id: 10,
+    name: "Sporting Event",
+    date: "2025-05-10",
+    time: "17:00",
+    location: "Sun City Resort - North West Province"
   }
 ];
 module.exports = events;
