25e2000bff48c8f41ad9d8b66a138e57141913f2
NeoPolori <143517527+NeoPolori@users.noreply.github.com>
2025-05-14 18:15:34 +0200

    Added home page css, event display and routers.

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -3,16 +3,34 @@
 const express = require('express');
 const router = express.Router();
 
+//Sample data
+const teamMembers = [
+                      { name: 'Jean Paul Smit', role: 'Developer' },
+                      {  name:'Vaughn Du Preez', role: 'Designer' },
+                      { name: 'Neo Polori', role: 'Manager' },
+                      { name: 'Joseph Pereira', role: 'Team Leader' },
+                    ]
+
+const events = [
+                 {title:'Summer festival', date:'2025-07-12', location:'Braamfontein'},
+                 {title:'Youth workshop', date:'2025-06-16', location:'Sandton'}
+               ]
+
+
+ //Store form data              ]
+const contactSubmission = [];
+
+               //Routes
 router.get('/', (req, res) => {
-    res.render('pages/home', { title: 'Home', message: 'Welcome to the Home Page' });
+    res.render('pages/home', { title: 'Home', message: 'Welcome to the Home Page', UpcomingEvents: events.slice(0.2) });//SHow first 2 events
 });
 
 router.get('/about', (req, res) => {
-    res.render('pages/about', { title: 'About', message: 'Welcome to the About Page' });
+    res.render('pages/about', { title: 'About', message: 'Welcome to the About Page', teamMembers });
 });
 
 router.get('/events', (req, res) => {
-    res.render('pages/events', { title: 'Events', message: 'Welcome to the Events Page' });
+    res.render('pages/events', { title: 'Events', message: 'Welcome to the Events Page', events });
 });
 
 router.get('/contact', (req, res) => {
@@ -21,8 +39,11 @@ router.get('/contact', (req, res) => {
 
 router.post('/thankyou', (req, res) => {
     const { name, email, message } = req.body;
+    contactSubmission.push({ name, email, message });//store submission 
     
     res.render('pages/thankyou', { title: 'Thank You', name, email, message });
 });
+
+
 
 module.exports = router;
